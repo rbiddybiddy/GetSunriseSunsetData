bd86653e16aa030dcee34f5f78d698534202dd45
Roland Couture <Roland.Couture@jacobs.com>
2023-04-13 11:53:54 -0700

    convert project to .NET 7 and use the DateOnly and TimeOnly structs

--- GetSunriseSunsetData/Program.cs
+++ GetSunriseSunsetData/Program.cs
@@ -13,23 +13,22 @@ namespace GetSunriseSunsetData
 		private static string _uriTemplate = "https://api.sunrise-sunset.org/json?lat={0}&lng={1}&date={2}";
 		private static string _lat = "44.95897";
 		private static string _lng = "-124.0145";
-		private static DateTime _startDate = new DateTime(2023, 1, 1);
+		private static DateOnly _startDate = new DateOnly(2023, 1, 1);
 		private static int _days = 10;
 		private static HttpClient _client = new HttpClient();
 
 		static async Task Main()
 		{
-			DateTime date;
-			DateTime sunrise;
-			DateTime sunset;
+			DateOnly date;
+			TimeOnly sunrise;
+			TimeOnly sunset;
 			Uri uri;
 			string responseJSON;
 
-			//var dataPoints = new List<DataPoint>();
-			var daysTable = new System.Data.DataTable("Days");
-			daysTable.Columns.Add(new DataColumn("date", typeof(DateTime))); //TODO - this didn't work out ideally format-wise, tempted to use string, see 2023-04-12-191447_SunriseSunsetData.xlsx
-			daysTable.Columns.Add(new DataColumn("sunrise", typeof(DateTime)));
-			daysTable.Columns.Add(new DataColumn("sunset", typeof(DateTime)));
+			var daysTable = new DataTable("Days");
+			daysTable.Columns.Add(new DataColumn("date", typeof(DateOnly)));
+			daysTable.Columns.Add(new DataColumn("sunrise", typeof(TimeOnly)));
+			daysTable.Columns.Add(new DataColumn("sunset", typeof(TimeOnly)));
 
 			for (int i = 0; i < _days; i++)
 			{
@@ -59,27 +58,28 @@ namespace GetSunriseSunsetData
 				{
 					JsonElement root = doc.RootElement;
 					JsonElement results = root.GetProperty("results");
-					sunrise = DateTime.Parse(results.GetProperty("sunrise").ToString()).ToLocalTime();
-					sunset = DateTime.Parse(results.GetProperty("sunset").ToString()).ToLocalTime();
+					sunrise = TimeOnly.FromDateTime(DateTime.Parse(results.GetProperty("sunrise").ToString()).ToLocalTime());
+					sunset = TimeOnly.FromDateTime(DateTime.Parse(results.GetProperty("sunset").ToString()).ToLocalTime());
 				}
 
 				daysTable.Rows.Add(new Object[] { date, sunrise, sunset });
-				Console.WriteLine($"{daysTable.Rows.Count} data points retrieved");
+				Console.WriteLine($"{daysTable.Rows.Count} data points retrieved" + Environment.NewLine);
 			}
 
 			//write output to Excel
 			var wbk = new XLWorkbook();
 			var sht = wbk.AddWorksheet(daysTable, "data");
-			//TODO - this worked great but still same formatting problem; and setting Range.Style.NumberFormat.NumberFormatId to (30 for m/d/yy) or (18 for /h:mm AM/PM) corrupted the file
-			//See 2023-04-12-194349_SunriseSunsetData.xlsx
+			sht.Columns(1, 3).AdjustToContents();
+			Console.WriteLine(Environment.NewLine + "Data written to Excel" + Environment.NewLine);
 
 			//save Excel file
 			string outputFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 			string timestamp = DateTime.Now.ToString("yyyy-MM-dd-HHmmss");
 			string outputFilePath = Path.Join(Path.Join(outputFolder, timestamp + "_SunriseSunsetData.xlsx"));
-			Console.WriteLine($"About to save to {outputFilePath}");
+			Console.WriteLine($"{Environment.NewLine}About to save to {outputFilePath}");
 			wbk.SaveAs(outputFilePath);
 			wbk.Dispose();
+			Console.WriteLine("Excel file saved" + Environment.NewLine);
 		}
 	}
 }
